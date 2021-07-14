38e7150587198d6531c0f25fe893065c6f4007f3
Anush M <69090449+anmutechsupport@users.noreply.github.com>
2021-07-14 14:32:43 -0400

    loading mock data

--- pyProcessing/preprocessing.py
+++ pyProcessing/preprocessing.py
@@ -3,5 +3,9 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import scipy 
 import brainflow
+import pickle 
 
+with open("mockdata\museeeg.pkl", "rb") as infile:
+    eeg = pickle.load(infile)
 
+print(len(eeg["finalData"]))
