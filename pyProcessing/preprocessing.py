f4b88c1313665160ddce651494247cc795ef8a14
Anush M <69090449+anmutechsupport@users.noreply.github.com>
2021-07-15 00:23:22 -0400

    gg

--- pyProcessing/preprocessing.py
+++ pyProcessing/preprocessing.py
@@ -11,6 +11,14 @@ with open("mockdata\museeeg.pkl", "rb") as infile:
 # order of array [af7, af8, tp9, tp10]
 fs = int(eeg["fs"])
 input_arr = np.array([x for x in eeg["finalData"]])
+k = 5
+shift = 2
 
-print(input_arr.shape)
-print("hello world")
+print(input_arr.shape[1]/fs)
+print(int(((input_arr.shape[1]/fs-k)/shift)+1))
+
+instF = []
+for n in range(int(((input_arr.shape[1]/fs-k)/shift)+1)):
+    instF.append(input_arr[:, n*shift*fs:(n*shift+k)*fs])
+
+print(np.array(instF).shape)
