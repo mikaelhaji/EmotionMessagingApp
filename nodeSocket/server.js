4fff911d0e735cdafd5d4a4c9b027ed33e1db1b6
Anush M <69090449+anmutechsupport@users.noreply.github.com>
2021-08-13 01:55:10 -0400

    adding multiroom functionality

--- nodeSocket/server.js
+++ nodeSocket/server.js
@@ -1,8 +1,10 @@
-// const io = require('socket.io')(3000)
-// io.set('origins', 'http://127.0.0.1:5500/Realtime-Simple-Chat-App-master');
-// const httpServer = require("http").createServer();
+const path = require('path')
+const http = require('http')
+const express = require('express')
+const app = express()
+const server = http.createServer(app);
 
-const io = require("socket.io")(3000, {
+const io = require("socket.io")(server, {
     rejectUnauthorized: false,
     cors: {
       origin: "*",
@@ -11,22 +13,50 @@ const io = require("socket.io")(3000, {
     allowEIO3: true
   });
   
-  const users = {}
+// app.set("views", "./views")
+console.log(path.join(__dirname, 'emotidetection'))
+app.use(express.static('emotidetection'))
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/', (req, res) => {
+  res.render('roomselection', { rooms: rooms })
+})
+
+// app.post('/room', (req, res) => {
+//   if (rooms[req.body.room] != null) {
+//     return res.redirect('/')
+//   }
+//   rooms[req.body.room] = { users: {} }
+//   res.redirect(req.body.room)
+//   // Send message that new room was created
+//   io.emit('room-created', req.body.room)
+// })
+
+app.get('/:room', (req, res) => {
+  // if (rooms[req.params.room] == null) {
+  //   return res.redirect('/')
+  // }
+  res.render('index', { roomName: req.params.room })
+})
+
+server.listen(3000)
   
-  io.on('connection', socket => {
-    socket.on('new-user', name => {
-      // console.log(name)
-      users[socket.id] = name
-      socket.broadcast.emit('user-connected', name)
-    })
-    socket.on('send-chat-message', (data) => {
-      console.log(data)
-      socket.broadcast.emit('chat-message', { message: data.message, name: users[socket.id], color: data.color})
-    })
-    socket.on('disconnect', () => {
-      socket.broadcast.emit('user-disconnected', users[socket.id])
-      delete users[socket.id]
-    })
+const users = {}
+
+io.on('connection', socket => {
+  socket.on('new-user', name => {
+    // console.log(name)
+    users[socket.id] = name
+    socket.broadcast.emit('user-connected', name)
   })
-  
+  socket.on('send-chat-message', (data) => {
+    console.log(data)
+    socket.broadcast.emit('chat-message', { message: data.message, name: users[socket.id], color: data.color})
+  })
+  socket.on('disconnect', () => {
+    socket.broadcast.emit('user-disconnected', users[socket.id])
+    delete users[socket.id]
+  })
+})
+
   // httpServer.listen(3000);
