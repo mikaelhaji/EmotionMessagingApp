26e5861f75741c627e5f9fec2f8abd872109a562
Mikael Haji <mikaelhaji@gmail.com>
2021-08-15 18:17:45 -0400

    Hello From the Other Side

--- emotidetection/app/UI.js
+++ emotidetection/app/UI.js
@@ -78,6 +78,18 @@ class UI{
           <meta http-equiv="X-UA-Compatible" content="ie=edge">
           <title>Chat App</title>
           <link rel="stylesheet" href="style.css">
+
+
+          <meta charset="utf-8">
+          <meta http-equiv="X-UA-Compatible" content="IE=edge">
+          <meta name="viewport" content="width=device-width, initial-scale=1">
+          <title>P300-based BCI Stimulus Presentation Paradigm</title>
+          
+          <link href="custom.css" rel="stylesheet">
+          <link rel="stylesheet" href="css/bootstrap/bootstrap.min.css">
+          <link rel="stylesheet" href="css/jquery/jquery-ui.css">
+
+          
         </head>
         <body>
    
@@ -91,7 +103,78 @@ class UI{
             <button type="submit" id="send-button">Send</button>
             <button style='position: relative;' type="button", id="devicebutton" class="brainsatplay-default-button">Connect BCI</button>
           </form>
-       
+
+          <div id="speller_matrix">
+	
+          <table>
+            <tr>
+              <td id="A">A</td>
+              <td id="B">B</td> 
+              <td id="C">C</td>
+              <td id="D">D</td>
+              <td id="E">E</td> 
+              <td id="F">F</td>
+            </tr>
+          
+            <tr>
+              <td id="G">G</td>
+              <td id="H">H</td> 
+              <td id="I">I</td>
+              <td id="J">J</td>
+              <td id="K">K</td> 
+              <td id="L">L</td>
+            </tr>
+          
+            <tr>
+              <td id="M">M</td>
+              <td id="N">N</td> 
+              <td id="O">O</td>
+              <td id="P">P</td>
+              <td id="Q">Q</td> 
+              <td id="R">R</td>
+            </tr>
+            
+            <tr>
+              <td id="S">S</td>
+              <td id="T">T</td> 
+              <td id="U">U</td>
+              <td id="V">V</td>
+              <td id="W">W</td> 
+              <td id="X">X</td>
+            </tr>
+            
+            <tr>
+              <td id="Y">Y</td>
+              <td id="Z">Z</td> 
+              <td id="0">0</td>
+              <td id="1">1</td>
+              <td id="2">2</td> 
+              <td id="3">3</td>
+            </tr>
+            
+            <tr>
+              <td id="4">4</td>
+              <td id="5">5</td> 
+              <td id="6">6</td>
+              <td id="7">7</td>
+              <td id="8">8</td> 
+              <td id="9">9</td>
+            </tr>
+          
+          </table>
+          
+          </div>
+          
+          <div>
+          
+            <button class="btn-primary btn-lg" id="start">START</button>
+          
+          </div>
+          <script src="js/jsmin//jquery.min.js"></script>
+          <script src="js/jsui/jquery-ui.min.js"></script>
+          
+          <script src="functions.js"></script>
+            
         
         </body>
 
@@ -318,3 +401,145 @@ class UI{
     }
 }
 export {UI}
+
+
+
+
+// $(document).ready(function() {		
+// 	$("#start").click( 
+// 		function() {
+			
+// 			number_of_trials = 5;
+			
+// 			var all_chars = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36];
+// 			new_chars = shuffle(all_chars);
+// 			number_of_trials--;
+			
+// 			for(a=0; a<number_of_trials; a++) {
+// 				temp_chars = shuffle(all_chars);
+// 				new_chars = new_chars.concat(temp_chars);
+// 			}
+						
+// 			c=new_chars.length;
+// 			i=0;
+			
+// 			var d = new Date();
+// 			var m = d.getMinutes();
+// 			var s = d.getSeconds();
+// 			var n = d.getMilliseconds();
+// 			console.log(m*60*1000+1000*s+n); // output second+ms to console log
+// 			setTimeout(flash,2000);
+// 			// 2 second pause before stimulus presentation starts
+			
+// 			var flash_time = 100;
+// 			var ISI = 100;
+			
+// 			function flash() {
+				
+// 				var d = new Date();
+// 				var m = d.getMinutes();
+// 				var s = d.getSeconds();
+// 				var n = d.getMilliseconds();
+// 				console.log(m*60*1000+1000*s+n); // output second+ms to console log
+											
+// 				if(i<c) {
+					
+// 					var flash_index = new_chars[i];
+					
+// 					light_unlit(flash_index,1); // highlight element
+					
+// 					setTimeout(
+// 						function() {
+// 							light_unlit(flash_index,0); // revert element to default colour after flash
+							
+// 							var d = new Date();
+// 							var m = d.getMinutes();
+// 							var s = d.getSeconds();
+// 							var n = d.getMilliseconds();
+// 							console.log(m*60000+1000*s+n); // output second+ms to console log
+							
+// 							setTimeout(flash,ISI);
+// 						}
+// 					,flash_time);
+					
+// 				}
+			
+// 				i++;
+			
+// 			}
+// 			// recursive function to keep calling setTimeout until all characters have flashed
+						
+// 			function light_unlit(char_index,state) {
+				
+// 				if(state==0) {
+// 					stim_colour = "white";
+// 				} else {
+// 					stim_colour = "red";
+// 				}
+				
+// 				switch(char_index) {
+// 				case 1: $("#A").css("color",stim_colour); break;
+// 				case 2: $("#B").css("color",stim_colour); break;
+// 				case 3: $("#C").css("color",stim_colour); break;
+// 				case 4: $("#D").css("color",stim_colour); break;
+// 				case 5: $("#E").css("color",stim_colour); break;
+// 				case 6: $("#F").css("color",stim_colour); break;
+// 				case 7: $("#G").css("color",stim_colour); break;
+// 				case 8: $("#H").css("color",stim_colour); break;
+// 				case 9: $("#I").css("color",stim_colour); break;
+// 				case 10: $("#J").css("color",stim_colour); break;
+// 				case 11: $("#K").css("color",stim_colour); break;
+// 				case 12: $("#L").css("color",stim_colour); break;
+// 				case 13: $("#M").css("color",stim_colour); break;
+// 				case 14: $("#N").css("color",stim_colour); break;
+// 				case 15: $("#O").css("color",stim_colour); break;
+// 				case 16: $("#P").css("color",stim_colour); break;
+// 				case 17: $("#Q").css("color",stim_colour); break;
+// 				case 18: $("#R").css("color",stim_colour); break;
+// 				case 19: $("#S").css("color",stim_colour); break;
+// 				case 20: $("#T").css("color",stim_colour); break;
+// 				case 21: $("#U").css("color",stim_colour); break;
+// 				case 22: $("#V").css("color",stim_colour); break;
+// 				case 23: $("#W").css("color",stim_colour); break;
+// 				case 24: $("#X").css("color",stim_colour); break;
+// 				case 25: $("#Y").css("color",stim_colour); break;
+// 				case 26: $("#Z").css("color",stim_colour); break;
+// 				case 27: $("#0").css("color",stim_colour); break;
+// 				case 28: $("#1").css("color",stim_colour); break;
+// 				case 29: $("#2").css("color",stim_colour); break;
+// 				case 30: $("#3").css("color",stim_colour); break;
+// 				case 31: $("#4").css("color",stim_colour); break;
+// 				case 32: $("#5").css("color",stim_colour); break;
+// 				case 33: $("#6").css("color",stim_colour); break;
+// 				case 34: $("#7").css("color",stim_colour); break;
+// 				case 35: $("#8").css("color",stim_colour); break;
+// 				case 36: $("#9").css("color",stim_colour); break;
+// 				default: 
+// 				}
+			
+// 			}
+			
+// 			function shuffle(array) {
+// 				var currentIndex = array.length, temporaryValue, randomIndex;
+
+// 				// While there remain elements to shuffle...
+// 				while (0 !== currentIndex) {
+
+// 					// Pick a remaining element...
+// 					randomIndex = Math.floor(Math.random() * currentIndex);
+// 					currentIndex -= 1;
+
+// 					// And swap it with the current element.
+// 					temporaryValue = array[currentIndex];
+// 					array[currentIndex] = array[randomIndex];
+// 					array[randomIndex] = temporaryValue;
+// 				}
+
+// 				return array;
+// 			}
+					
+// 		}
+				
+// 	);
+	
+// });
